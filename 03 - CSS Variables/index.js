7d47f4d57238cfbd1c160e38e93147c5d6a10e56
Penghua <kids5346@gmail.com>
2020-03-13 12:15:03 +0800

    private variables

--- 03 - CSS Variables/index.js
+++ 03 - CSS Variables/index.js
@@ -1,12 +1,21 @@
-const styleObj = {
-  space: 10,
-  blur: 1,
-  color: "#ffc600",
-  current: null,
-};
 
-
+const styleAccessor = new StyleObjAccessor();
 initEvents();
+
+function StyleObjAccessor() {
+  const styleObj = {
+    space: 10,
+    blur: 1,
+    color: "#ffc600",
+    current: null,
+  };
+  this.setStyleValue = (name, value) => {
+    styleObj['current'] = name;
+    styleObj[name] = value;
+  };
+  this.getStyle = fn => fn(styleObj);
+}
+
 function initEvents() {
   document
     .querySelector('.controls')
@@ -20,21 +29,16 @@ function setObjValue(e) {
   if(e.target.nodeName !== 'INPUT') return;
   let idName = e.target.id;
   let inputValue = e.target.value;
-  styleObj['current'] = idName;
-  styleObj[idName] = inputValue;
-  changeCssVariablesValues();
+  styleAccessor.setStyleValue(idName, inputValue);
+  styleAccessor.getStyle(changeCssStyle);
 }
 
-function changeCssVariablesValues() {
+function changeCssStyle(styleObj) {
   if(styleObj.current !== 'color') {
-    document
-      .documentElement
-      .style
+    document.documentElement.style
       .setProperty(`--${ styleObj.current }`, `${ styleObj[styleObj.current] }px`);
     return;
   }
-  document
-    .documentElement
-    .style
+  document.documentElement.style
     .setProperty(`--${ styleObj.current }`, `${ styleObj[styleObj.current] }`);    
 }
