5f4bde6e1d5655e823682f2b94bc14d475762227
Penghua <kids5346@gmail.com>
2020-03-16 21:08:43 +0800

    [refactor]: code arrage

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,21 +1,19 @@
 const store = new StoreAccessor();
-
+const checkboxs = document.querySelectorAll('input[type="checkbox"]');
+checkboxs
+  .forEach(checkbox => checkbox.addEventListener('click', clickHandler));
+  
 function StoreAccessor() {
   let storeArray = [];
 
   this.getStoreArray = () => storeArray;
-  this.updateStoreArray = (storeArray ,index) => {
+  this.updateStoreArray = (storeArray, index) => {
     storeArray.length >= 2 
       ? storeArray.pop() 
       : null
     storeArray.push(index);
   };
 }
-
-const checkboxs = document.querySelectorAll('input[type="checkbox"]');
-
-checkboxs
-  .forEach(checkbox => checkbox.addEventListener('click', clickHandler));
 
 function clickHandler(e) {
   let isShift = e.shiftKey;
@@ -31,6 +29,7 @@ function updateCheckedStatus(isShift) {
     store.getStoreArray().sort((a, b) => a - b);
     const [startIndex, endIndex] = store.getStoreArray();
     const targetElements = [...checkboxs].slice(startIndex, endIndex + 1);
+
     targetElements.forEach(element => element.checked = true);    
   }
 }
